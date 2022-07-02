103bfd162639da8e48e27cbbb953ad8f5c097cab
Aleksandar Bayrev <aleksandarbayrev@gmail.com>
2022-07-02 20:12:23 +0300

    Move index.css to styles folder

--- src/frontend/src/index.tsx
+++ src/frontend/src/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
+import './styles/index.css';
 import { App } from './components/App';
 import _ from 'lodash';
 import reportWebVitals from './reportWebVitals';
